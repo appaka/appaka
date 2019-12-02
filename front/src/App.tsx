13d909438505fa322d002a0a60bc0491d24d4d0c
Vlad Flonta <vlad.flonta@ufirstgroup.ch>
2019-12-02 19:44:31 +0100

    Add layout and navigation from example, install dependencies on node startup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,25 @@
 import React from "react";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import "./App.css";
+import Layout from "./layout";
 import Home from "./components/home";
 import Tasks from "./components/tasks";
 import Stories from "./components/stories";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/tasks">Tasks</Link>
-            </li>
-            <li>
-              <Link to="/stories">Stories</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route exact path="/" component={Home} />
-        <Route path="/tasks" component={Tasks} />
-        <Route path="/stories" component={Stories} />
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <CssBaseline/>
+            <Router>
+                <Layout>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/tasks" component={Tasks}/>
+                    <Route path="/stories" component={Stories}/>
+                </Layout>
+            </Router>
+        </>
+    );
 };
 
 export default App;
---
+++ front/src/layout/index.tsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import clsx from 'clsx';
+import {createStyles, makeStyles, useTheme, Theme} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import {Link} from "react-router-dom";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+        },
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: 36,
+        },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+            whiteSpace: 'nowrap',
+        },
+        drawerOpen: {
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        drawerClose: {
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            overflowX: 'hidden',
+            width: theme.spacing(7) + 1,
+            [theme.breakpoints.up('sm')]: {
+                width: theme.spacing(9) + 1,
+            },
+        },
+        toolbar: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+        },
+        menuItem: {
+            textDecoration: 'none',
+            color: 'inherit',
+        }
+    }),
+);
+
+export default function Layout({children}: { children: React.ReactNode }) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open,
+                        })}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        AppAka
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbar}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                    </IconButton>
+                </div>
+                <Divider/>
+                {[
+                    {label: 'Home', to: '/', icon: <HomeIcon/>},
+                    {label: 'Tasks', to: '/tasks', icon: <AssignmentIndIcon/>},
+                    {label: 'Stories', to: '/stories', icon: <InboxIcon/>},
+                ].map(({label, to, icon}) => (
+                    <Link to={to} key={label} className={classes.menuItem}>
+                        <ListItem button>
+                            <ListItemIcon>{icon}</ListItemIcon>
+                            <ListItemText primary={label}/>
+                        </ListItem>
+                    </Link>
+                ))}
+                <Divider/>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
+                {children}
+            </main>
+        </div>
+    );
+}
